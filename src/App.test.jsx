eb2208c452c6ae1cb6da257485c0c1177b0d32ce
Miriama Svitkova <miriama.svitkova@reaktor.com>
2026-02-27 13:15:04 +0100

    Add ESLint + Prettier
    - ESLint 9 flat config with react-hooks and react-refresh plugins
    - Prettier configured to match existing style
    - Added lint and format scripts to package.json
    - Applied Prettier formatting to src/ files

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders the recipe search input', () => {
+test("renders the recipe search input", () => {
   render(<App />);
   expect(screen.getByPlaceholderText(/search recipes/i)).toBeInTheDocument();
 });
 
-test('renders the recipes heading', () => {
+test("renders the recipes heading", () => {
   render(<App />);
   expect(screen.getByText(/all time flavourite/i)).toBeInTheDocument();
 });
--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -17,9 +17,7 @@ function Recipes() {
       >
         All time flavourite
       </h2>
-      <div className="row row-cols-1 row-cols-md-4 g-3">
-        {recipeComponents}
-      </div>
+      <div className="row row-cols-1 row-cols-md-4 g-3">{recipeComponents}</div>
     </div>
   );
 }
